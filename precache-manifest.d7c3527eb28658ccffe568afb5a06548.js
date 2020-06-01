6b0ac66b13b8d8c0d940dc019c9aabbb1cd7becc
rishabh0211 <rishabh0211@gmail.com>
2020-06-01 23:43:36 +0530

    Updates

---
+++ precache-manifest.d7c3527eb28658ccffe568afb5a06548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcca970a1885d694f07b6009ac4d6649",
+    "url": "/index.html"
+  },
+  {
+    "revision": "607dcaa86c0367c45235",
+    "url": "/static/css/main.6e2fd95c.chunk.css"
+  },
+  {
+    "revision": "c266ef24dcb12edcf535",
+    "url": "/static/js/2.59fad772.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.59fad772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "607dcaa86c0367c45235",
+    "url": "/static/js/main.547981d7.chunk.js"
+  },
+  {
+    "revision": "b524bc42875bca629429",
+    "url": "/static/js/runtime-main.62172fe8.js"
+  }
+]);
